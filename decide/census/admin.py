2ba86bbe332e1dca50ad4a6320b455a5d8165fa8
cramersito <carlosalbalat97@gmail.com>
2020-01-10 12:40:06 +0000

    [refactor](census)Comprobar voting_id sea correcto
    Closes #91

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Census
+from voting.models import Voting
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
 
 
 class CensusAdmin(admin.ModelAdmin):
@@ -8,6 +12,36 @@ class CensusAdmin(admin.ModelAdmin):
     list_filter = ('voting_id', )
 
     search_fields = ('voter_id', )
+
+    def check_user_exist(self, username):
+        User = get_user_model()
+
+        if User.objects.filter(username=username).exists():
+            return True
+        else:
+            return False
+
+    def save_model(self, request, obj, form, change):
+        """
+        Given a model instance save it to the database.
+
+        """
+
+
+        votings = Voting.objects.all()
+
+        voting_id = obj.voting_ide
+
+        if votings:
+
+            for v in votings:
+                 if v.id == voting_id and self.check_user_exist(request.user.username):
+
+                    obj.save()
+                    break
+                 else:
+
+                     return redirect('http://localhost:8000/admin/census/census/')
 
 
 admin.site.register(Census, CensusAdmin)
--- decide/census/models.py
+++ decide/census/models.py
@@ -1,9 +1,27 @@
 from django.db import models
-
+from voting.models import Voting
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
     voter_id = models.PositiveIntegerField()
 
+    def check_save(self,id):
+
+        votings = Voting.objects.all()
+
+        if votings:
+
+            for v in votings:
+                if v.id == id:
+                    break
+                    return True
+                else:
+
+                    return False
+
+
+
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
+
+
