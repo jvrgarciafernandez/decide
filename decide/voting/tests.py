d2cc314201affb64782fa58a5a8f8ddf1a9fa0e6
Fernando <fernandoruizp97@gmail.com>
2020-01-10 18:55:24 +0000

    [style](voting) Reformato de líneas en archivos
    Se ha vuelto a realizar una formateo de líneas al salir los test erróneos.
    
    Closes #100

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -233,8 +233,7 @@ class VotingTestCase(BaseTestCase):
     def test_check_inputFile(self):
 
         THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-        filePath = THIS_FOLDER 
-        + '/docs/CandidatesFiles/Candidatos_Senado.xlsx'
+        filePath = THIS_FOLDER + '/docs/CandidatesFiles/Candidatos_Senado.xlsx'
         # my_file = os.path.join(THIS_FOLDER + '/docs/CandidatesFiles/',
         #  'Candidatos_Senado.xlsx')
         
@@ -242,16 +241,14 @@ class VotingTestCase(BaseTestCase):
         Voting.checkInputFile(filePath)
 
         # Test negativo. Un candidato no ha pasado por el proceso de primarias
-        filePath = THIS_FOLDER 
-        + '/docs/CandidatesFiles/Candidatos_Senado2.xlsx'
+        filePath = THIS_FOLDER + '/docs/CandidatesFiles/Candidatos_Senado2.xlsx'
         try:
             Voting.checkInputFile(filePath)
         except:
             print('Test negativo de proceso de primarias correcto')
         
         # Test negativo. Faltan provincias con candidatos
-        filePath = THIS_FOLDER 
-        + '/docs/CandidatesFiles/Candidatos_Senado3.xlsx'
+        filePath = THIS_FOLDER + '/docs/CandidatesFiles/Candidatos_Senado3.xlsx'
         try:
             Voting.checkInputFile(filePath)
         except:
@@ -259,10 +256,9 @@ class VotingTestCase(BaseTestCase):
         
         # Test negativo. No hay 6 candidatos/provincia/partido político 
         # ni relación 1/2 entre hombres y mujeres
-        filePath = THIS_FOLDER 
-        + '/docs/CandidatesFiles/Candidatos_Senado4.xlsx'
+        filePath = THIS_FOLDER + '/docs/CandidatesFiles/Candidatos_Senado4.xlsx'
         try:
             Voting.checkInputFile(filePath)
         except:
             print('Test negativo de 6 candidatos/provincia/partido político'+ 
-            'y relación 1/2 correcto')
+            'yrelación 1/2 correcto')
